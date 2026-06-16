9a5c7f4d92ea098f64a061dc450965ded6768d45
Riddhima Gupta <riddhima303@gmail.com>
2026-06-16 18:14:33 +0530

    pop-up added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import { useState } from "react";
+import "./App.css";
 
-const App = () => {
-  return (
-    <div id="paper">
-      <canvas id="drawingCanvas"></canvas>
+export default function App() {
+const [showPopup, setShowPopup] = useState(true);
+
+return ( 
+  <div id="paper">
+  <canvas id="drawingCanvas"></canvas>
+  {showPopup && (
+    <div className="overlay">
+      <div className="cat-window">
+      <div className="title-bar">Scribble-Space</div>
+      <div className="window-content">
+      <p>You have received a cat.</p>
+      <img src="/cat.png" alt="cat" className="cat-img" />
+      <button className="ok-btn" onClick={() => setShowPopup(false)}>OK</button>
+      </div>
+      </div>
     </div>
-  );
-};
-
-export default App;
+  )}
+</div>
+);
+}
