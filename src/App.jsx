da829f7048571e45bd7bcbdae09eebd45826f41b
Riddhima Gupta <riddhima303@gmail.com>
2026-06-17 17:14:50 +0530

    cat on the screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,69 @@
 import { useState } from "react";
 import "./App.css";
 import catImage from "./cat.png";
+
 export default function App() {
 const [showPopup, setShowPopup] = useState(true);
+const [showCat, setShowCat] = useState(false);
+const [catPos, setCatPos] = useState({ x: 0, y: 0 });
 
-return ( 
-  <div id="paper">
-  <canvas id="drawingCanvas"></canvas>
+const receiveCat = () => {
+setShowPopup(false);
+
+
+setCatPos({
+  x: Math.random() * (window.innerWidth - 120),
+  y: Math.random() * (window.innerHeight - 120),
+});
+
+setShowCat(true);
+
+
+};
+
+return ( <div id="paper"> <canvas id="drawingCanvas"></canvas>
+
+
   {showPopup && (
     <div className="overlay">
       <div className="cat-window">
-      <div className="title-bar">Scribble-Space</div>
-      <div className="window-content">
-      <p>You have received a cat.</p>
-      <img src={catImage} alt="cat" className="cat-img" />
-      <button className="ok-btn" onClick={()  => setShowPopup(false)}>OK</button>
-      </div>
+        <div className="title-bar">
+          Scribble-Space
+        </div>
+
+        <div className="window-content">
+          <p>You have received a cat.</p>
+
+          <img
+            src={catImage}
+            alt="cat"
+            className="cat-img"
+          />
+
+          <button
+            className="ok-btn"
+            onClick={receiveCat}
+          >
+            OK
+          </button>
+        </div>
       </div>
     </div>
   )}
+
+  {showCat && (
+    <img
+      src={catImage}
+      alt="cat"
+      className="screen-cat"
+      style={{
+        left: `${catPos.x}px`,
+        top: `${catPos.y}px`,
+      }}
+    />
+  )}
 </div>
+
+
 );
 }
